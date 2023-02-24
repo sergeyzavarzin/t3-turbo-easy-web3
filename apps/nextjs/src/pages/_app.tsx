9bff7806e4c8dcbf1e16b4aaa07543a6c4b3cfdf
Sergey Zavarzin <sergey@superdao.co>
2023-02-24 16:35:08 +0400

    feat: add thirdweb provider & example rpc class

--- apps/nextjs/src/pages/_app.tsx
+++ apps/nextjs/src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { ThirdwebProvider } from "@thirdweb-dev/react";
 import type { AppType } from "next/app";
 import { Web3AuthProvider } from "~/contexts/Web3AuthContext";
 import { api } from "~/utils/api";
@@ -6,7 +7,9 @@ import "../styles/globals.css";
 const App: AppType = ({ Component, pageProps: { ...pageProps } }) => {
   return (
     <Web3AuthProvider>
-      <Component {...pageProps} />
+      <ThirdwebProvider>
+        <Component {...pageProps} />
+      </ThirdwebProvider>
     </Web3AuthProvider>
   );
 };
